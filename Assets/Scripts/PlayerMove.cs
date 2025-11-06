a1006b1c046abcf031133cda06ac2e1eec439406
Emilio Jimenez <emiliojimenez@Emilios-MacBook-Pro.local>
2025-11-05 17:30:10 -0800

    added sprite states

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -7,12 +7,24 @@ public class PlayerMove : MonoBehaviour
     private Rigidbody2D rb;
     private InputSystem_Actions input;
 
+    // Sprites for different states. Used in FixedUpdate
+    public Sprite frontSprite;
+    public Sprite backSprite;
+
+    public Sprite leftSprite;
+    public Sprite rightSprite;
+
+    private SpriteRenderer spriteRenderer;
+
     private Vector2 moveInput;
+
+   
 
     void Awake()
     {
         input = new InputSystem_Actions();
         rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
 
         //Listen for movement input
         input.Player.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
@@ -37,5 +49,15 @@ public class PlayerMove : MonoBehaviour
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + moveInput * speed * Time.fixedDeltaTime);
-    }
+
+        // Change sprite based on movement direction
+        if (moveInput.y > 0)
+            spriteRenderer.sprite = backSprite;
+        else if (moveInput.y < 0)
+            spriteRenderer.sprite = frontSprite;
+        else if (moveInput.x > 0)
+            spriteRenderer.sprite = rightSprite;
+        else if (moveInput.x < 0)
+            spriteRenderer.sprite = leftSprite;
+    }       
 }
